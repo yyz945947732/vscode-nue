9a97af3dd1bc9523ae365aa57f9b987c8dfc3980
yyz945947732 <945947732@qq.com>
2023-10-18 12:05:37 +0800

    build: add commitlint

--- src/extension.js
+++ src/extension.js
@@ -3,20 +3,20 @@ const vscode = require('vscode');
 // This hack is necessary to ensure that the vscode.html-language-features extension is activated in order to enable
 // htmlLanguageParticipants support. Please see https://github.com/microsoft/vscode/issues/160585 for more info.
 async function activate() {
-	const htmlExtension = vscode.extensions.getExtension('vscode.html-language-features');
+  const htmlExtension = vscode.extensions.getExtension('vscode.html-language-features');
 
-	if (!htmlExtension) {
-		const output = vscode.window.createOutputChannel('nue');
+  if (!htmlExtension) {
+    const output = vscode.window.createOutputChannel('nue');
 
-		output.appendLine(
-			'Warning: Could not find vscode.html-language-features. HTML Language Participants support will be disabled.',
-		);
-		return;
-	}
+    output.appendLine(
+      'Warning: Could not find vscode.html-language-features. HTML Language Participants support will be disabled.'
+    );
+    return;
+  }
 
-	await htmlExtension?.activate();
+  await htmlExtension?.activate();
 }
 
 module.exports = {
-	activate,
+  activate,
 };
